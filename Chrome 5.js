edbf56d5d1f8f57682fd25ee9b0393d14a0da35a
jacknam05 <81426591+jacknam05@users.noreply.github.com>
2021-04-17 23:43:41 +0700

    Add files via upload

---
+++ Chrome 5.js
@@ -0,0 +1,5 @@
+let object = [{name="Jack", age:"16"}, {name="Giang", age:"15"}, {name="Trung", age:"15"}, {name="Nam Anh", age:"15"}, {name="Hoang", age:"15"}, {name="Dung", age:"15"}, {name="Khoi", age:"15"}, {name="Huy", age:"15"}, {name="Lan", age:"15"}, {name="Vinh", age:"15"}, {name="Son", age:"15"}, {name="Khoa", age:"15"}, {name="Trung", age:"15"}, {name="Minh", age:"15"}]
+console.log(object)
+for(let i = 0; i>=1000; i++/3) {
+    console.log(i)
+}
